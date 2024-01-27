5c4cedcc947d5527cbc35b67766264b34fee9652
Enrique990 <voidenrique@gmail.com>
2024-01-27 12:25:30 -0600

    primer ejecicio

--- 1.cpp
+++ 1.cpp
@@ -1,8 +1,16 @@
-#include<iostream>
+//Create a program that takes in the weight of an item and then calculates how much that item would weigh on Mars.
+
+#include <iostream>
 using namespace std;
 
-int main(int argc, char const *argv[])
-{
-    cout << "Hola mundo";
-    return 0;
+int main() {
+  int weight;
+  double weightM;
+
+  cout << "Introduce a weight in kg: ";
+  cin >> weight;
+  weightM = weight * 0.37;
+  cout << weight << " KG" << " in Mars are: " << weightM << " KG";
+
+  return 0;
 }
